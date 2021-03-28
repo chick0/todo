edad304d32fcdad5284adbee81d28db0b9610828
chick_0 <chick_0@ch1ck.xyz>
2021-03-29 05:18:27 +0900

    투두 api화 작업

---
+++ app/views/api.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+from json import dumps
+
+from flask import Blueprint
+from flask import request, session
+from flask import url_for
+from flask import Response
+
+from app import db
+from models import Member, Todo
+
+
+bp = Blueprint(
+    name=__name__.split(".")[-1],
+    import_name=__name__,
+    url_prefix=f"/{__name__.split('.')[-1]}"
+)
+
+
+def get_member():
+    return Member.query.filter_by(
+        idx=session.get("user_idx", -1)
+    ).first()
+
+
+@bp.route("")
+def get():
+    member = get_member()
+    if member is None:
+        return Response(
+            status=401,
+            mimetype="application/json",
+            response=dumps({
+                "error": "인증 실패, 로그인이 필요합니다!"
+            })
+        )
+
+    try:
+        page = int(request.args.get("page", "1"))
+
+        if page <= 0:
+            page = 1
+    except ValueError:
+        page = 1
+
+    todo = Todo.query.filter_by(
+        owner=member.idx
+    ).order_by(
+        Todo.idx.desc()
+    ).paginate(page)
+
+    return Response(
+        status=200,
+        mimetype="application/json",
+        response=dumps({
+            "page": page,
+            "prev": url_for("todo.dashboard", page=todo.prev_num),
+            "next": url_for("todo.dashboard", page=todo.next_num),
+            "todo": [
+                dict(
+                    idx=item.idx,
+                    text=item.text
+                ) for item in todo.items]
+        })
+    )
+
+
+@bp.route("/append", methods=['POST'])
+def append():
+    member = get_member()
+    if member is None:
+        return Response(
+            status=401,
+            mimetype="application/json",
+            response=dumps({
+                "error": "인증 실패, 로그인이 필요합니다!"
+            })
+        )
+
+    text = request.form.get("todo", "").strip()
+    if len(text) != 0:
+        todo = Todo()
+        todo.owner = member.idx
+        todo.text = text
+
+        db.session.add(todo)
+        db.session.commit()
+
+        return Response(
+            status=201,
+            mimetype="application/json",
+            response=dumps({
+                "alert": "추가 완료"
+            })
+        )
+    else:
+        return Response(
+            status=200,
+            mimetype="application/json",
+            response=dumps({
+                "alert": "할 일을 입력해야 합니다"
+            })
+        )
+
+
+@bp.route("/pop")
+def pop():
+    member = get_member()
+    if member is None:
+        return Response(
+            status=401,
+            mimetype="application/json",
+            response=dumps({
+                "error": "인증 실패, 로그인이 필요합니다!"
+            })
+        )
+
+    try:
+        idx = int(request.args.get("idx", "-1"))
+
+        if idx <= 0:
+            raise ValueError
+    except ValueError:
+        return Response(
+            status=400,
+            mimetype="application/json",
+            response=dumps({
+                "error": "잘못된 요청 입니다"
+            })
+        )
+
+    todo = Todo.query.filter_by(
+        idx=idx,
+        owner=member.idx
+    ).first()
+
+    if todo is None:
+        return Response(
+            status=404,
+            mimetype="application/json",
+            response=dumps({
+                "error": "삭제할 투두를 찾지 못했습니다"
+            })
+        )
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return Response(
+        status=200,
+        mimetype="application/json",
+        response=dumps({
+            "alert": "삭제 완료"
+        })
+    )
--- app/views/todo.py
+++ app/views/todo.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-from json import dumps
 
 from flask import Blueprint
 from flask import request, session
 from flask import redirect, url_for
 from flask import render_template
-from flask import Response
 
 from app import db
 from models import Member, Todo
@@ -24,7 +22,7 @@ def get_member():
     ).first()
 
 
-@bp.route("/dashboard")
+@bp.route("")
 def dashboard():
     member = get_member()
     if member is None:
@@ -38,105 +36,9 @@ def dashboard():
     except ValueError:
         page = 1
 
-    todo = Todo.query.filter_by(
-        owner=member.idx
-    ).order_by(
-        Todo.idx.desc()
-    ).paginate(page)
-
     return render_template(
         "todo/dashboard.html",
-        todo=todo
-    )
-
-
-@bp.route("/append", methods=['POST'])
-def append():
-    member = get_member()
-    if member is None:
-        return Response(
-            status=401,
-            mimetype="application/json",
-            response=dumps({
-                "error": "인증 실패, 로그인이 필요합니다!"
-            })
-        )
-
-    text = request.form.get("todo", "").strip()
-    if len(text) != 0:
-        todo = Todo()
-        todo.owner = member.idx
-        todo.text = text
-
-        db.session.add(todo)
-        db.session.commit()
-
-        return Response(
-            status=201,
-            mimetype="application/json",
-            response=dumps({
-                "alert": "추가 완료"
-            })
-        )
-    else:
-        return Response(
-            status=200,
-            mimetype="application/json",
-            response=dumps({
-                "alert": "할 일을 입력해야 합니다"
-            })
-        )
-
-
-@bp.route("/pop")
-def pop():
-    member = get_member()
-    if member is None:
-        return Response(
-            status=401,
-            mimetype="application/json",
-            response=dumps({
-                "error": "인증 실패, 로그인이 필요합니다!"
-            })
-        )
-
-    try:
-        idx = int(request.args.get("idx", "-1"))
-
-        if idx <= 0:
-            raise ValueError
-    except ValueError:
-        return Response(
-            status=400,
-            mimetype="application/json",
-            response=dumps({
-                "error": "잘못된 요청 입니다"
-            })
-        )
-
-    todo = Todo.query.filter_by(
-        idx=idx,
-        owner=member.idx
-    ).first()
-
-    if todo is None:
-        return Response(
-            status=404,
-            mimetype="application/json",
-            response=dumps({
-                "error": "삭제할 투두를 찾지 못했습니다"
-            })
-        )
-
-    db.session.delete(todo)
-    db.session.commit()
-
-    return Response(
-        status=200,
-        mimetype="application/json",
-        response=dumps({
-            "alert": "삭제 완료"
-        })
+        page=page
     )
 
 
@@ -151,4 +53,4 @@ def clear():
 
     db.session.commit()
 
-    return redirect(url_for(".dashboard"))
+    return redirect(url_for("todo.dashboard"))
