7908ef1cbf503ef55cb6dd77e9738aa6aa4798c7
chick_0 <chick_0@kakao.com>
2021-03-30 15:52:45 +0900

    api 구조 변경

--- app/views/api.py
+++ app/views/api.py
@@ -23,8 +23,8 @@ def get_member():
     ).first()
 
 
-@bp.route("")
-def get():
+@bp.route("/todo", methods=['GET', 'POST', 'DELETE'])
+def todo():
     member = get_member()
     if member is None:
         return Response(
@@ -44,140 +44,97 @@ def get():
             })
         )
 
-    try:
-        page = int(request.args.get("page", "1"))
+    if request.method == "GET":
+        try:
+            page = int(request.args.get("page", "1"))
 
-        if page <= 0:
+            if page <= 0:
+                page = 1
+        except ValueError:
             page = 1
-    except ValueError:
-        page = 1
 
-    todo = Todo.query.filter_by(
-        owner=member.idx
-    ).order_by(
-        Todo.idx.desc()
-    ).paginate(page)
+        td = Todo.query.filter_by(
+            owner=member.idx
+        ).order_by(
+            Todo.idx.desc()
+        ).paginate(page)
 
-    return Response(
-        status=200,
-        mimetype="application/json",
-        response=dumps({
-            "page": page,
-            "prev": url_for("todo.dashboard", page=todo.prev_num),
-            "next": url_for("todo.dashboard", page=todo.next_num),
-            "todo": [
-                dict(
-                    idx=item.idx,
-                    text=item.text,
-                    date=item.register.isoformat()
-                ) for item in todo.items]
-        })
-    )
-
-
-@bp.route("/append", methods=['POST'])
-def append():
-    member = get_member()
-    if member is None:
-        return Response(
-            status=401,
-            mimetype="application/json",
-            response=dumps({
-                "error": "인증 실패, 로그인이 필요합니다!"
-            })
-        )
-
-    if len(member.secret) != 0 and not session.get("2fa_status", False):
-        return Response(
-            status=401,
-            mimetype="application/json",
-            response=dumps({
-                "error": "요청 거부, 2단계 인증을 통과한 상태가 아닙니다"
-            })
-        )
-
-    text = request.form.get("todo", "").strip()
-    if len(text) != 0:
-        todo = Todo()
-        todo.owner = member.idx
-        todo.text = text
-
-        db.session.add(todo)
-        db.session.commit()
-
-        return Response(
-            status=201,
-            mimetype="application/json",
-            response=dumps({
-                "alert": "추가 완료"
-            })
-        )
-    else:
         return Response(
             status=200,
             mimetype="application/json",
             response=dumps({
-                "alert": "할 일을 입력해야 합니다"
+                "page": page,
+                "prev": url_for("todo.dashboard", page=td.prev_num),
+                "next": url_for("todo.dashboard", page=td.next_num),
+                "todo": [
+                    dict(
+                        idx=item.idx,
+                        text=item.text,
+                        date=item.register.isoformat()
+                    ) for item in td.items]
             })
         )
+    elif request.method == "POST":
+        text = request.form.get("todo", "").strip()
+        if len(text) != 0:
+            td = Todo()
+            td.owner = member.idx
+            td.text = text
 
+            db.session.add(td)
+            db.session.commit()
 
-@bp.route("/pop")
-def pop():
-    member = get_member()
-    if member is None:
+            return Response(
+                status=201,
+                mimetype="application/json",
+                response=dumps({
+                    "alert": "추가 완료"
+                })
+            )
+        else:
+            return Response(
+                status=200,
+                mimetype="application/json",
+                response=dumps({
+                    "alert": "할 일을 입력해야 합니다"
+                })
+            )
+    elif request.method == "DELETE":
+        try:
+            idx = int(request.args.get("idx", "-1"))
+
+            if idx <= 0:
+                raise ValueError
+        except ValueError:
+            return Response(
+                status=400,
+                mimetype="application/json",
+                response=dumps({
+                    "error": "잘못된 요청 입니다"
+                })
+            )
+
+        td = Todo.query.filter_by(
+            idx=idx,
+            owner=member.idx
+        ).first()
+
+        if td is None:
+            return Response(
+                status=404,
+                mimetype="application/json",
+                response=dumps({
+                    "error": "삭제할 투두를 찾지 못했습니다"
+                })
+            )
+
+        db.session.delete(td)
+        db.session.commit()
+
         return Response(
-            status=401,
+            status=200,
             mimetype="application/json",
             response=dumps({
-                "error": "인증 실패, 로그인이 필요합니다!"
+                "alert": "삭제 완료"
             })
         )
-
-    if len(member.secret) != 0 and not session.get("2fa_status", False):
-        return Response(
-            status=401,
-            mimetype="application/json",
-            response=dumps({
-                "error": "요청 거부, 2단계 인증을 통과한 상태가 아닙니다"
-            })
-        )
-
-    try:
-        idx = int(request.args.get("idx", "-1"))
-
-        if idx <= 0:
-            raise ValueError
-    except ValueError:
-        return Response(
-            status=400,
-            mimetype="application/json",
-            response=dumps({
-                "error": "잘못된 요청 입니다"
-            })
-        )
-
-    todo = Todo.query.filter_by(
-        idx=idx,
-        owner=member.idx
-    ).first()
-
-    if todo is None:
-        return Response(
-            status=404,
-            mimetype="application/json",
-            response=dumps({
-                "error": "삭제할 투두를 찾지 못했습니다"
-            })
-        )
-
-    db.session.delete(todo)
-    db.session.commit()
-
-    return Response(
-        status=200,
-        mimetype="application/json",
-        response=dumps({
-            "alert": "삭제 완료"
-        })
-    )
